deefdfd2d4613c9b578c63608d0fb87359220b5a
surachok <38752974+surachokh@users.noreply.github.com>
2022-10-19 10:56:28 +0700

    Update: Split handle function into useEffect

--- src/Pages/CustomerPage.tsx
+++ src/Pages/CustomerPage.tsx
@@ -4,7 +4,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { Form, Input, Table } from "antd";
 
 export default function CustomerPage() {
-  const [page, setPage] = useState(3);
+  const [page, setPage] = useState(1);
   const [pageSize, setPageSize] = useState(10);
   const [formValues, setFormValues] = useState([{ value: null }]);
 
@@ -14,10 +14,45 @@ export default function CustomerPage() {
     skip: (page - 1) * 10,
     limit: pageSize,
   };
+
   const dispatch = useDispatch();
+
   useEffect(() => {
     dispatch({ type: "REQUEST_CUSTOMER", initPayload });
   }, []);
+
+  useEffect(() => {
+    const payload = {
+      skip: (page - 1) * 10,
+      limit: pageSize,
+    };
+
+    if (formValues[0]?.value !== "" && formValues[0]?.value)
+      Object.assign(payload, { code: formValues[0].value });
+    if (formValues[1]?.value !== "" && formValues[1]?.value)
+      Object.assign(payload, { name: formValues[1].value });
+    if (formValues[2]?.value !== "" && formValues[2]?.value)
+      Object.assign(payload, { phone: formValues[2].value });
+
+    dispatch({ type: "REQUEST_CUSTOMER", payload });  
+  }, [page, pageSize])
+
+  useEffect(() => {
+    const payload = {
+      skip: 0,
+      limit: 10,
+    };
+
+    if (formValues[0]?.value !== "" && formValues[0]?.value)
+      Object.assign(payload, { code: formValues[0].value });
+    if (formValues[1]?.value !== "" && formValues[1]?.value)
+      Object.assign(payload, { name: formValues[1].value });
+    if (formValues[2]?.value !== "" && formValues[2]?.value)
+      Object.assign(payload, { phone: formValues[2].value });
+
+    dispatch({ type: "REQUEST_CUSTOMER", payload });  
+  }, [formValues])
+  
 
   const columns = [
     {
@@ -44,40 +79,11 @@ export default function CustomerPage() {
 
   const handleChange = (_?: any, values?: any) => {
     setFormValues(values);
-
-    const payload = {
-      skip: "0",
-      limit: "10",
-    };
-
-    //condition for filter
-    if (values[0].value !== "" && values[0].value)
-      Object.assign(payload, { code: values[0].value });
-    if (values[1].value !== "" && values[1].value)
-      Object.assign(payload, { name: values[1].value });
-    if (values[2].value !== "" && values[2].value)
-      Object.assign(payload, { phone: values[2].value });
-
-    dispatch({ type: "REQUEST_CUSTOMER", payload });
   };
 
   const handlePaginationChange = (page: number, pageSize: number) => {
     setPage(page)
     setPageSize(pageSize)
-
-    const payload = {
-      skip: (page - 1) * 10,
-      limit: pageSize,
-    };
-
-    if (formValues[0]?.value !== "" && formValues[0]?.value)
-      Object.assign(payload, { code: formValues[0].value });
-    if (formValues[1]?.value !== "" && formValues[1]?.value)
-      Object.assign(payload, { name: formValues[1].value });
-    if (formValues[2]?.value !== "" && formValues[2]?.value)
-      Object.assign(payload, { phone: formValues[2].value });
-
-    dispatch({ type: "REQUEST_CUSTOMER", payload });
   };
 
   return (
